bc01d5077587d7ec2d037930bb4907c282e8c7d4
Shannon <a12023808@unet.univie.ac.at>
2025-03-06 17:11:17 +0100

    bla New commit

--- Setup.py
+++ Setup.py
@@ -1,2 +1,18 @@
+import numba
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy
+
 print('hi')
 print('test2')
+
+x = np.array([1, 2, 3, 4, 10, 20, 30, 40])
+y = np.sum(x)
+print(y)
+
+plt.plot(x)
+plt.title('Sample Plot')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.show()
